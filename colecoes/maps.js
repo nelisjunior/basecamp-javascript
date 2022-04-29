723fb933ca8ed07c8e9d9a095d82a24b6b3ae386
Nelis Junior <nelisxxi@gmail.com>
2022-04-28 23:06:02 -0300

    minhas modificações

--- colecoes/maps.js
+++ colecoes/maps.js
@@ -10,12 +10,26 @@ const getAdmins = (map) => {
 	return admins;
 };
 
+const getMods = (map) => {
+	let mods = [];
+
+	for ([key, value] of map) {
+		if (value === 'MOD') {
+			mods.push(key);
+		}
+	}
+
+	return mods;
+};
+
 const userRoles = new Map();
 
-userRoles.set('Stephany', 'SUDO');
-userRoles.set('Luiz', 'ADMIN');
-userRoles.set('Elvira', 'ADMIN');
-userRoles.set('Carolina', 'USER');
+userRoles.set('Nelis', 'SUDO');
+userRoles.set('Darly', 'ADMIN');
+userRoles.set('Gaspar', 'MOD');
 userRoles.set('Guilherme', 'USER');
+userRoles.set('Natasha', 'USER');
+userRoles.set('Alice', 'USER');
 
-console.log(getAdmins(userRoles));
+console.log("Administradores: " + getAdmins(userRoles));
+console.log("Moderadores: " + getMods(userRoles));
